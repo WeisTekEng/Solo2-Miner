efdcd4a4483218c5681a6b7f444e1fc0f00c7816
OCybress <OCybress@gmail.com>
2025-11-18 21:57:45 -0800

    Add configuration file for WiFi and mining settings

---
+++ src/configs.h
@@ -0,0 +1,13 @@
+
+// Wifi
+#define WIFI_SSID "your_ssid" // 2.5Ghz
+#define WIFI_PASSWORD "your_password"
+
+// Mining
+#define THREADS 2
+#define MAX_NONCE 0xFFFFFFFF
+#define ADDRESS "bc1qpe8gjgfs5hh0aw7veusxqppycyz0ea0nvjxr3k"
+
+// Pool
+#define POOL_URL "solo.ckpool.org"
+#define POOL_PORT 3333
